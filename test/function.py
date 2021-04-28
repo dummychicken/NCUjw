4d47b8c20ed260ceacf04cd611275e8ed55e85a8
guolong Tan <guolongtan@163.com>
2021-04-28 11:36:09 +0800

    fix

---
+++ test/function.py
@@ -0,0 +1,165 @@
+import csv 
+import re 
+import pandas as pd
+import numpy as np
+import numba as nb
+
+from config import Config as cf
+from sqlalchemy import create_engine
+
+period = cf.get_period
+
+# 按照每门课程的考试班级数目进行排序
+def SamplingCourse(file):
+    with open(file,"r",encoding="UTF-8") as f:
+        reader = csv.reader(f)
+        column = [row[0] for row in reader]
+    hashmapClass = {}
+    for i in range(len(column)):
+        hashmapClass[column[i]] = hashmapClass.get(column[i],0) + 1
+    sortedCourse = sorted(hashmapClass.items(), key=lambda d:d[1], reverse = True)
+    return sortedCourse[15:500]
+
+# 找出共同能考试的时间
+def findCommonTime(temp, listClass):
+    commonTime = []
+    for i in range(len(temp)):
+        commonTime.append(temp[i] & listClass[i])
+    return commonTime
+
+# 尽可能使考试平均分布
+def optimalTime(temp):
+    pt1 = 0
+    pt2 = len(temp)
+    while pt1 != pt2-1:
+        mid = ( pt1 + pt2 ) // 2 
+        if sum(temp[pt1:mid]) < sum(temp[mid:pt2]):
+            pt1 = mid
+        else:
+            pt2 = mid
+    if sum(temp[pt1: pt1+2]) <= 1:
+        return temp.index(1)
+    else:
+        return pt1
+
+# 将结果写入表中
+def writeToCsv(time, course, classes, number, teacher, college):
+    course = [course] * len(classes)
+    time = [time] * len(classes)
+    college = [college[0]] * len(classes)
+    test = pd.DataFrame({'课程': course, "考试时间": time, "班级": classes,"班级人数": number, "监考老师": teacher, "开课学院": college})
+    test.to_csv("test\\temp.csv",encoding = "utf-8",mode="a",index = None, header = False)
+
+# 讲已经排考成功的1修改成0, 如[1,1,1,1]->[0,1,1,1] 维护表
+def updateClassTime(dict, listclass, index):
+    for classes in listclass:
+        dict[classes][index] = 0
+    return dict
+
+def updateTeacherTime(dict, listclass, index,di):
+    for classes in listclass:
+        if classes == "-1":
+            break
+        dict[classes][index] = 0
+    return dict
+
+# 函数的效果是对班级进行统计, 目的是每个班级必须有一个考试时间表 
+# 比如 "行政管理[193-4]班,人工智能191班" -> ['行政管理193班', '行政管理194班', '人工智能191班']
+def unitedClass(strClass):
+    listClasses = strClass.split(",")
+    splitClassList = []
+    for classes in listClasses:
+        if "-" in classes:
+            middleIndex = classes.index("-")
+            firstIndex = classes.index("[")
+            lastIndex = classes.index("]")
+            lastClassNum = int(classes[middleIndex + 1 :lastIndex])
+            firstClassNum = int(classes[middleIndex - 1])
+            for i in range( firstClassNum, lastClassNum + 1):
+                splitClassList.append(classes[0:firstIndex] + classes[firstIndex + 1:middleIndex-1] + str(i) + "班")
+        else:
+            splitClassList.append(classes)
+    return splitClassList
+
+# 数据预处理，输入排课表,根据规则拆分班级
+# '土木工程（建筑工程方向）[181-3]班,土木工程（地下工程方向）181班' ——>
+# '土木工程（建筑工程方向）181班,土木工程（建筑工程方向）182班,土木工程（建筑工程方向）183班,土木工程（地下工程方向）181班'   
+def pre_split_class(data):
+    class_name = data["ci_class_name"]
+    for inx, val in enumerate(class_name):
+        st = ''
+        for s in unitedClass(str(val)):
+            st = st + s + ","
+        data.iloc[inx,2] = st[:-1]
+
+# 这里是在定义拆分规则
+# 可以人数超出阈值, 则拆分班级, 如果人数没有超过, 就不拆
+# 可以避免合班的问题
+
+def splitClass(classWhoTakeCourse,classCourseNumber):
+    newClassWhoTakeCourse = []
+    newClassCourseNumber = []
+    for i in range(len(classCourseNumber)):
+        # 这个数字怎么订 还是要看教室的具体规模
+        if classCourseNumber[i] > 80:
+            classes = unitedClass(classWhoTakeCourse[i])
+            # 如果拆分出来的班级小于30人, 就没有必要拆
+            if classCourseNumber[i] // len(classes) <= 30:
+                newClassCourseNumber.append(classCourseNumber[i])
+                newClassWhoTakeCourse.append(classWhoTakeCourse[i])
+            else:
+                for unit in classes:
+                    newClassWhoTakeCourse.append(unit)
+                    # 这里需要插一段每个班级真正人数的查询
+                    # 这里是偷懒取了个平均 
+                    newClassCourseNumber.append(classCourseNumber[i]//len(classes))
+        else:
+            newClassCourseNumber.append(classCourseNumber[i])
+            newClassWhoTakeCourse.append(classWhoTakeCourse[i])
+    return newClassWhoTakeCourse, newClassCourseNumber
+
+
+
+# 建立每个key和value的联系
+# 比如 课程 和 老师
+def buildConnectionDict(data, key, value):
+    dictKeyValue = {}
+    for key,val in zip(data[key],data[value]):
+    #for i in range(len(data)):
+        keyName = key
+       #keyName = data.loc[i][key]
+        dictKeyValue[keyName] = dictKeyValue.get(keyName,[])
+        #valueName = data.loc[i][value]
+        valueName = val
+        if valueName is np.nan:
+            continue
+        elif ":" in valueName:
+            valueList = valueName.split(":")
+            for values in valueList:
+                dictKeyValue[keyName].append(values)
+        else:
+            dictKeyValue[keyName].append(valueName)
+    for key,value in dictKeyValue.items():
+        value = list(set(value))
+        dictKeyValue[key] =  value
+    return dictKeyValue
+
+# 记录不完美情况
+def writeImperfect(index,TeacherList,data):
+    rows = []
+    for i in range(len(index)):
+        if TeacherList[i] not in data.iloc[index[i]]["ci_teacher_name"]:
+            writeUnitRow = []
+            writeUnitRow.append(index[i])
+            for unit in range(len(data.iloc[0])):
+                writeUnitRow.append(data.iloc[index[i]][unit])
+            rows.append(writeUnitRow)
+    with open("test\imperfect.csv","a",encoding = "utf-8",newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(rows)
+
+
+def getDF(sql):   # 'mysql+pymysql://root:123456@123.60.8.234:3306/examArrange1'
+    engin = create_engine('mysql+pymysql://root:ncu@jw114@123.60.11.177:3306/examArrange1')
+    df = pd.read_sql_query(sql, engin)
+    return df
---
+++ test/jw.py
@@ -0,0 +1,146 @@
+import csv
+import re
+import time as ti
+
+import numpy as np
+import pandas as pd
+
+import function as fn
+from config import Config as cf
+
+
+st_time = ti.time()
+period = cf.get_period()
+limited = cf.get_limited()
+threshold = cf.get_threshold()
+
+#sql_data = """
+            #select ci_course_no,ci_class_name,
+            #ci_student_number,ci_teacher_name,ci_class_dep
+            #from classs_inf
+            #"""
+
+data = pd.read_csv("test\class.csv", usecols=["ci_course_no","ci_class_name","ci_student_number","ci_teacher_name","ci_class_dep"])
+
+#data = fn.getDF(sql_data)
+
+# 数据预处理模板
+fn.pre_split_class(data) # 拆分同一行内按类似“1-3”的班级
+
+# 建立每个老师和时间的联系
+dictTeacherTime = {}
+for cl_ter_name in data["ci_teacher_name"]:
+#for i in range(len(data)):
+    #teacher = data.loc[i]["ci_teacher_name"]
+    teacher = cl_ter_name
+    if teacher is np.nan:
+        continue
+    elif ":" in teacher:
+        teacherlist = teacher.split(":")
+        for teachers in teacherlist:
+            dictTeacherTime[teachers] = dictTeacherTime.get(teachers,[1]*period)
+    else:
+        dictTeacherTime[teacher] = dictTeacherTime.get(teacher,[1]*period)
+
+
+# 建立所有学院和老师的联系
+dataCollege = pd.read_csv("test\class.csv", usecols=["ci_class_dep","ci_teacher_name"])
+dictCollegeTeacher = fn.buildConnectionDict(dataCollege, "ci_class_dep","ci_teacher_name")
+# 建立课程和学院的联系
+dataCourse = pd.read_csv("test\class.csv", usecols=["ci_class_dep","ci_course_no"])
+dictCourseCollege = fn.buildConnectionDict(dataCourse,"ci_course_no","ci_class_dep")
+# 建立课程和老师的联系
+dataCourseTeacher = pd.read_csv("test\class.csv", usecols=["ci_course_no","ci_teacher_name"])
+dictCourseTeacher = fn.buildConnectionDict(dataCourseTeacher,"ci_course_no","ci_teacher_name")
+
+# 生成一个字典，对应的形式是这样的{"班级1":[],"班级2":[],"班级3":[],...}
+dictClassTime = {}
+for courseNumber in fn.SamplingCourse("test\class.csv"):
+    course = courseNumber[0]
+    temp = [1] * period
+    tempTeacher = [1] * period
+    classWhoTakeCourse = []
+    classCourseNumber = []
+    unitClass = []
+    classTeacherList = []
+    indexList = []
+
+    for i,cl_name,cl_num,cl_ter,cl_co in zip(data.index,data["ci_class_name"],data["ci_student_number"],data["ci_teacher_name"],data["ci_course_no"].values):
+        # 查找每一个考这门课的班级
+        if cl_co == course:
+            className = cl_name
+            classNumber = cl_num
+            # 保存班级人数和班级的时候以整体为单位
+            classWhoTakeCourse.append(className)
+            classCourseNumber.append(classNumber)
+            indexList.append(i)
+
+            # 取每个班级的公共时间
+            splitClassList = fn.unitedClass(className) 
+            for className in splitClassList:
+                # 对考这门课的班级的时间进行初始化, 如果不存在就生成一个[1,1,1,1...,]
+                dictClassTime[className] = dictClassTime.get(className,[1] * period)
+                # 找到所有参加这门考试 班级的共同空闲时间
+                # [0,1,1,...,] 和 [1,0,1,...,] 相与
+                temp = fn.findCommonTime(temp,dictClassTime[className])
+                unitClass.append(className)
+            
+            classTeacher = cl_ter
+            classTeacherList.append(classTeacher)
+            
+            # 接下取监考老师的公共时间
+            unitTeacher = []
+            classTeacher = cl_ter
+            if ":" in classTeacher:
+                teachersList = classTeacher.split(":")
+                for teacher in teachersList:
+                    tempTeacher = fn.findCommonTime(tempTeacher, dictTeacherTime[teacher])
+                    unitTeacher.append(teacher)
+            else:
+                tempTeacher = fn.findCommonTime(tempTeacher,dictTeacherTime[classTeacher])
+                unitTeacher.append(classTeacher)
+    
+    # 去除重复老师
+    unitTeacher = list(set(unitTeacher))
+    
+
+    # 两个公共时间相与
+    temp = fn.findCommonTime(tempTeacher,temp)
+    
+    # 判断监考老师是否足够, 不够从同学院找
+    if len(unitTeacher) < courseNumber[1]:
+        college = dictCourseCollege[course]
+        collegeTeachers = dictCollegeTeacher[college[0]]
+        for Teacher in collegeTeachers:
+            if Teacher not in unitTeacher and fn.findCommonTime(dictTeacherTime[Teacher], tempTeacher) == tempTeacher:
+                unitTeacher.append(Teacher)
+
+    # 监考老师多了, 就选到只有这么多老师, 非常粗糙         
+    if len(unitTeacher) > courseNumber[1]:
+        unitTeacher = unitTeacher[0:courseNumber[1]]
+    
+    di = 0
+    if len(unitTeacher) < courseNumber[1]:
+        di = courseNumber[1] - len(unitTeacher)
+        print(courseNumber[0]+"课程，当前老师不足，缺少"+courseNumber[0]+"个，用-1代替")
+        di_list = ["-1"]*di
+        unitTeacher.extend(di_list)
+
+
+    # 记录没有授课老师的情况
+    #fn.writeImperfect(indexList,unitTeacher,data)
+
+    if 1 not in temp:
+        print("当前" + course +"无法排考")
+    # 找到共同可行的时间, 写入csv, 并且标记占用时间
+    else:
+        _time = fn.optimalTime(temp)
+        # newClassWhoTakeCourse, newClassCourseNumber = splitClass(classWhoTakeCourse,classCourseNumber)
+        college = dictCourseCollege[course]
+        fn.writeToCsv(_time,course,classWhoTakeCourse,classCourseNumber, unitTeacher,college)      
+        # writeToCsv(temp,course,classWhoTakeCourse,classCourseNumber,college)              
+        # 更新老师班级的时间表
+        fn.updateClassTime(dictClassTime, unitClass, _time)
+        fn.updateTeacherTime(dictTeacherTime, unitTeacher, _time,di)
+
+print(ti.time() - st_time)
