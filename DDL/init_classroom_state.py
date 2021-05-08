5680f36c7dea050e7cbf4aa5697c10ae1f3eaac0
guolongtan <52573340+guolongtan1@users.noreply.github.com>
2021-05-08 16:28:36 +0800

    Add files via upload

--- DDL/init_classroom_state.py
+++ DDL/init_classroom_state.py
@@ -1,8 +1,20 @@
 import function as fn
+import argparse
 
-def main():
-    fn.init_classroomState()
-    print("init_ok")
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--building",
+                    default="教学主楼,外经楼,法学楼,人文楼,建工楼,机电楼,信工楼,材料楼,环境楼,理生楼",
+                    type=str,
+                    required=False,
+                    help='''输入要重置楼栋资源名称(例如：教学主楼,外经楼,法学楼,人文楼,
+                    建工楼,机电楼,信工楼,材料楼,环境楼,理生楼),支持多资源输入(例如: 教学主楼,外经楼 )''')
+
+args = parser.parse_args()
+
+def main(args):
+    fn.init_classroomState(args.building)
+    print(args.building,"init_ok")
 
 if __name__ == "__main__":
-    main()
+    main(args)
